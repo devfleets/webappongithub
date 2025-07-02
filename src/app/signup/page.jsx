d740bb42e956b060fb144b0513af0869211a72e0
DevFleets <devfleets@gmail.com>
2025-07-02 20:49:43 +0530

    morechanges

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,5 +1,5 @@
-// "use client"
-// import { useState } from "react";
+"use client"
+import { useState } from "react";
  import { useRouter } from "next/navigation";
 // import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 // import { auth } from "@/firebase/firebase";
